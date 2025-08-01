5d48eaa60fd287fb511196905ae75fdf5f24bce2
Oliver Xu <olxu@hotmail.com>
2025-08-01 16:03:28 +1000

    1

---
+++ 1b.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void debug_out() { cerr << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+    cerr << " " << H;
+    debug_out(T...);
+}
+
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+
+const ll MOD = (ll)(1e9)+7ll;
+const ll INF = (1ll << 60);
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  int n;
+  cin >> n;
+  vector<int> a(n), b(n);
+  for (int i = 0; i < n; i++) {
+    cin >> a[i] >> b[i];
+  }
+  map<int, int> freq;
+  for (int x : b) {
+    freq[x]++;
+  }
+  long long sum = 0;
+  for (int x : a) {
+    sum += (long long) x * freq[x];
+  }
+  cout << sum << endl;
+  return 0;
+}
