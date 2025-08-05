4f5df5ac3baf01c0b105f371a55f4f55cd35038b
Oliver Xu <olxu@hotmail.com>
2025-08-05 23:55:15 +1000

    3a

---
+++ 3a.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void debug_out() { cerr << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+    cerr << " " << H;
+    debug_out(T...);
+}
+
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+
+const ll MOD = (ll)(1e9)+7ll;
+const ll INF = (1ll << 60);
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  int n;
+  cin >> n;
+  string s, t;
+  for (int i = 0; i < n && cin >> t; s += t);
+  auto Num = [&](int& i) {
+    int x = 0;
+    while (isdigit(s[i])) {
+      x = 10 * x + (s[i] - '0');
+      i++;
+    }
+    return x;
+  };
+  int k = (int) s.size();
+  int ans;
+  for (int i = 0; i < k; i++) {
+    if (s[i] == 'm') {
+      if (i + 3 < k && s.substr(i+1, 3) == "ul(") {
+        i += 4;
+        int x = Num(i);
+        if (s[i] == ',') {
+          i++;
+          int y = Num(i);
+          if (s[i] == ')') {
+            ans += x * y;
+          }
+        }
+      }
+    }
+  }
+  cout << ans << '\n';
+  return 0;
+}
